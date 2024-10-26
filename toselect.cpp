cf42460cf300b13445582ec8804608c69569c2e4
Job Hernandez <jobhdezlara93@gmail.com>
2024-10-25 17:25:52 -0700

    refactor

--- toselect.cpp
+++ toselect.cpp
@@ -21,25 +21,49 @@ class Instructions {
 public:
   Instructions(const std::vector<std::shared_ptr<Instruction>> &instructions)
       : instructions(instructions) {}
+
   std::vector<std::shared_ptr<Instruction>> get_instructions() const {
     return instructions;
+  }
+
+  void print_instructions() const {
+    std::cout << "{";
+
+    for (size_t i = 0; i < instructions.size(); ++i) {
+      std::cout << "{";
+
+      const auto &instr = instructions[i]->get_instruction();
+      for (size_t j = 0; j < instr.size(); ++j) {
+        std::cout << instr[j];
+        if (j < instr.size() - 1) {
+          std::cout << ", ";
+        }
+      }
+
+      std::cout << "}";
+      if (i < instructions.size() - 1) {
+        std::cout << ", ";
+      }
+    }
+
+    std::cout << "}" << std::endl;
   }
 };
 
 class InstructionSelector {
 public:
-  static std::vector<std::shared_ptr<Instruction>>
-  to_select(const std::shared_ptr<Expression> &anf) {
+  static Instructions to_select(const std::shared_ptr<Expression> &anf) {
     size_t counter = 0;
-    stack.clear(); // Clear the stack map for each new selection process.
-    return anf_to_select(anf, counter);
+    std::unordered_map<std::string, std::string> stack;
+    auto instructions = anf_to_select(anf, counter, stack);
+    return Instructions(instructions);
   }
 
 private:
-  static std::unordered_map<std::string, std::string> stack;
-
   static std::vector<std::shared_ptr<Instruction>>
-  anf_to_select(const std::shared_ptr<Expression> &anf, size_t &counter) {
+  anf_to_select(const std::shared_ptr<Expression> &anf, size_t &counter,
+                std::unordered_map<std::string, std::string> &stack) {
+
     std::vector<std::shared_ptr<Instruction>> instructions;
 
     if (std::shared_ptr<LetExpression> anf_let =
@@ -47,14 +71,7 @@ private:
       std::string let_var = anf_let->get_variable();
 
       if (stack.find(let_var) == stack.end()) {
-        ++counter;
-        ++counter;
-        ++counter;
-        ++counter;
-        ++counter;
-        ++counter;
-        ++counter;
-        ++counter;
+        counter += 8;
         std::string number_string = std::to_string(counter);
         std::string stack_location = "-" + number_string + "(%rbp)";
         stack[let_var] = stack_location;
@@ -66,8 +83,8 @@ private:
         std::vector<std::string> mov = {"movq", immediate, stack_location};
         instructions.push_back(std::make_shared<Instruction>(mov));
 
-        std::vector<std::shared_ptr<Instruction>> body_instructions =
-            anf_to_select(anf_let->get_body(), counter);
+        auto body_instructions =
+            anf_to_select(anf_let->get_body(), counter, stack);
         instructions.insert(instructions.end(), body_instructions.begin(),
                             body_instructions.end());
       } else {
@@ -80,8 +97,8 @@ private:
         std::vector<std::string> mov = {"movq", immediate, stack_location};
         instructions.push_back(std::make_shared<Instruction>(mov));
 
-        std::vector<std::shared_ptr<Instruction>> body_instructions =
-            anf_to_select(anf_let->get_body(), counter);
+        auto body_instructions =
+            anf_to_select(anf_let->get_body(), counter, stack);
         instructions.insert(instructions.end(), body_instructions.begin(),
                             body_instructions.end());
       }
@@ -100,37 +117,11 @@ private:
   }
 };
 
-std::unordered_map<std::string, std::string> InstructionSelector::stack;
-
-void print_instructions(
-    const std::vector<std::shared_ptr<Instruction>> &instructions) {
-  std::cout << "{";
-
-  for (size_t i = 0; i < instructions.size(); ++i) {
-    std::cout << "{";
-
-    const auto &instr = instructions[i]->get_instruction();
-    for (size_t j = 0; j < instr.size(); ++j) {
-      std::cout << instr[j];
-      if (j < instr.size() - 1) {
-        std::cout << ", ";
-      }
-    }
-
-    std::cout << "}";
-    if (i < instructions.size() - 1) {
-      std::cout << ", ";
-    }
-  }
-
-  std::cout << "}" << std::endl;
-}
-
 int main() {
-  std::string input = "(let ((x 1)) x)";
+  //  std::string input = "(let ((x 1)) x)";
+  std::string input = "(let ((x 1)) (let ((y 2)) x))";
   std::shared_ptr<Expression> ast = Parser::parse(input);
   std::shared_ptr<Expression> anf = ToAnf::to_anf(ast);
-  std::vector<std::shared_ptr<Instruction>> instructions =
-      InstructionSelector::to_select(anf);
-  print_instructions(instructions);
+  Instructions instructions = InstructionSelector::to_select(anf);
+  instructions.print_instructions();
 }
